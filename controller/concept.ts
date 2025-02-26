351640196686d278e6fdbff075542ab76ac885ef
Jonas Van Hoof <jonasvanhoof@inuits.eu>
2025-02-26 13:06:45 +0100

    lmb-1232: delete concepts with there implementations

--- controller/concept.ts
+++ controller/concept.ts
@@ -1,18 +1,20 @@
-import { query, sparqlEscapeString, sparqlEscapeUri } from 'mu';
+import { query, update, sparqlEscapeString, sparqlEscapeUri } from 'mu';
 
-export async function getConceptUri(conceptId: string) {
+export async function getConceptUris(conceptIds: Array<string>) {
+  const escapedIds = conceptIds.map((id) => sparqlEscapeString(id)).join('\n');
   const queryResult = await query(`
     PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
     PREFIX mu: <http://mu.semte.ch/vocabularies/core/>
 
-    SELECT ?concept
+    SELECT ?concept ?id
     WHERE {
+      VALUES ?id { ${escapedIds} }
       ?concept a skos:Concept.
-      ?concept mu:uuid ${sparqlEscapeString(conceptId)} .
-    } LIMIT 1
+      ?concept mu:uuid ?id .
+    }
   `);
 
-  return queryResult.results.bindings[0]?.concept?.value;
+  return queryResult.results.bindings.map((b) => b.concept?.value);
 }
 
 export async function findConceptImplementation(conceptUri: string) {
@@ -31,4 +33,36 @@ export async function findConceptImplementation(conceptUri: string) {
   return queryResult.results.bindings
     .map((b) => b.implementation?.value)
     .filter((i) => i);
+}
+
+export async function deleteConceptsWithImplementations(
+  conceptUris: Array<string>,
+) {
+  const escapedUris = conceptUris.map((uri) => sparqlEscapeUri(uri)).join('\n');
+  try {
+    await update(`
+      PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+  
+      DELETE {
+        ?concept ?cP ?cO .
+        ?implementation ?p ?concept .
+      }
+      WHERE {
+        VALUES ?concept { ${escapedUris} }
+  
+        ?concept a skos:Concept .
+        ?concept ?cP ?cO .
+  
+        OPTIONAL {
+          ?implementation ?p ?concept .
+        }
+      }
+    `);
+  } catch (error) {
+    throw {
+      message:
+        'Something went wrong while deleting the concepts and there implementations',
+      status: 500,
+    };
+  }
 }
--- router/concept.ts
+++ router/concept.ts
@@ -4,7 +4,8 @@ import { Request, Response } from 'express';
 
 import {
   findConceptImplementation,
-  getConceptUri,
+  getConceptUris,
+  deleteConceptsWithImplementations,
 } from '../controller/concept';
 
 export const conceptRouter = Router();
@@ -12,16 +13,16 @@ export const conceptRouter = Router();
 conceptRouter.get(
   '/:id/has-implementations',
   async (req: Request, res: Response) => {
-    const conceptUri = await getConceptUri(req.params.id);
+    const conceptUris = await getConceptUris([req.params.id]);
 
-    if (!conceptUri) {
+    if (conceptUris.length === 0) {
       throw {
         message: `Invalid concept id. Could not find concept with id: ${req.params.id}`,
         status: 400,
       };
     }
 
-    const implementations = await findConceptImplementation(conceptUri);
+    const implementations = await findConceptImplementation(conceptUris[0]);
 
     res.status(200).send({
       hasImplementations: implementations.length >= 1,
@@ -29,3 +30,17 @@ conceptRouter.get(
     });
   },
 );
+
+conceptRouter.delete('/ids', async (req: Request, res: Response) => {
+  const conceptUris = await getConceptUris(req.body.ids);
+
+  if (conceptUris.length === 0) {
+    throw {
+      message: 'No concept ids found in body',
+      status: 400,
+    };
+  }
+
+  await deleteConceptsWithImplementations(conceptUris);
+  res.status(204).send();
+});
